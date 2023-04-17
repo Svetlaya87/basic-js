56cf81fa7b686f791afeac69a72ee3f1baedde18
Svetlaya87 <svetlaya87@gmail.com>
2023-04-18 00:25:23 +0300

    fix: add param to func dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(arr) {
   let name = [];
     for(let i=0; i<arr.length; i++){
         if( typeof(arr[i]) == 'string' ){

85b516e8f2309ee3f219953c4f9db8adae2490ca
Svetlaya87 <svetlaya87@gmail.com>
2023-12-20 13:22:27 +0200

    feat: solve matrix elements um task

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  //matrix.flat(Infinity).every(el=> el ===0);
+   let childLength = matrix[0].length;
+ 
+   let res =[];
+   
+  for(let i =0; i< matrix.length-1; i++){
+        
+    for (let y=0; y< matrix[i].length; y++){
+        if(matrix[i][y]===0 || matrix[i][y] === 'delete'){
+
+           
+            matrix[i+1][y] = 'delete';
+     
+        }
+    }    
+
+  }
+    
+    
+    res= matrix.flat(Infinity).filter((el)=> el !== 'delete').reduce((acc, curVal)=> acc+curVal, 0);
+    console.log(res);
+   
+  
+
+   return res;
+  
 }
 
 module.exports = {

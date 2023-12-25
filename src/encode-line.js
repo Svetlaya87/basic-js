2e01a2a3e297e67b124d0cb43657a1f51db2f432
Svetlaya87 <svetlaya87@gmail.com>
2023-12-26 01:49:45 +0200

    fix: change solution to encode line js

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,24 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine( str ) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
   str = str.split('');
-  let filtered = str;
-  let res = '';
 
-  let str2 = Array.from(new Set(str));
-  for(let i=0; i< str2.length; i++){
-      let filtered2= filtered.filter(el=> el === str2[i]);
-      if(filtered2.length !==0){
-          res = res+filtered2.length + filtered2[0];
+  let acc=0;
+  let res = acc+'';
+  for(let i=0; i< str.length-1; i++){
+
+      
+      if( str[i] === str[i+1]){
+          acc = acc+1;
+          
       }
 
-      filtered = filtered.filter(el => el !== str[i]);
+      if( str[i] !== str[i+1]){
+          acc = acc+1;
+          res = res + acc+str[i];
+          acc=0;
+      } 
 
+      if( str[i] !== str[i+1] && i+1 === str.length-1){
+          
+          res = res +str[i+1];
+          
+      }
+
+      if( str[i] === str[i+1] && i+1 === str.length-1){
+          acc = acc+1;
+          res = res + acc+str[i+1];
+          acc=0;
+          
+      }
+      
+      
   }
-  res = res.split('').filter(el => el*1 !==1).join('');
 
+  res = res.split('').filter(el => el*1 !==1 && el*1!== 0).join('');
   return res;
 }
 
